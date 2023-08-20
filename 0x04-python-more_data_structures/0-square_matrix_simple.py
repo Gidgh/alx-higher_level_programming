c1679d47641fd377f7a94de47ac60679394c347a
Gid <gideonandrewedu@gmail.com>
2023-08-20 15:50:37 -0700

    0x04 python more data structures square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,18 +1,8 @@
-def square_matrix_simple(matrix=[]):
-    """
-    Computes the square value of all integers in a matrix.
-    
-    Args:
-        matrix (list[list[int]]): The input 2D matrix.
-        
-    Returns:
-        list[list[int]]: A new matrix with squared values.
-    """
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for num in row:
-            new_row.append(num ** 2)
-        new_matrix.append(new_row)
-    return new_matrix
+#!/usr/bin/python3
 
+def square_matrix_simple(matrix=[]):
+    new_matrix = [row[:] for row in matrix]
+    for idx, row in enumerate(new_matrix):
+        for idx2, col in enumerate(new_matrix):
+            new_matrix[idx][idx2] = row[idx2] ** 2
+    return new_matrix
