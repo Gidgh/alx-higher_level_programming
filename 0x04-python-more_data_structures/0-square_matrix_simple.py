99bb4edc1a946d54efb4da10bc81bb2cef202762
Gid <gideonandrewedu@gmail.com>
2023-08-20 15:57:40 -0700

    0x04 python more data structures square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [row[:] for row in matrix]
-    for idx, row in enumerate(new_matrix):
-        for idx2, col in enumerate(new_matrix):
-            new_matrix[idx][idx2] = row[idx2] ** 2
+    new_matrix = []
+    for row in matrix:
+        new_row = [value ** 2 for value in row]
+        new_matrix.append(new_row)
     return new_matrix
