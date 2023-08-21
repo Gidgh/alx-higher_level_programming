51a9b435511e8e8e97727f2cdab71e1c61167cf2
Gid <gideonandrewedu@gmail.com>
2023-08-21 08:19:05 -0700

    0x04 python more data structures

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 def complex_delete(a_dictionary, value):
     keys_to_delete = [key for key, val in a_dictionary.items() if val == value]
-    new_dict = {key: val for key, val in a_dictionary.items() if key not in keys_to_delete}
-    return new_dict
+    for key in keys_to_delete:
+        del a_dictionary[key]
+    return a_dictionary
